9391f956d1eb8c764c69b638cd7cb2f636e53ea7
KathiTram <49622903+KathiTram@users.noreply.github.com>
2022-06-22 21:04:11 -0700

    Work on logic for letters in wrong locations

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 words = ['which', 'other', 'three', 'eagle', 'shady', 'above', 'adult', 'bones', 'bonks', 'cones'];
 
+let charsOfWord;
+let charsOfChoice;
+
 const gridContainer = document.querySelector('.gridContainer');
 const box = document.querySelector('.box');
+
+function play() {
+    createBoxes(5);
+    pickRandomWord();
+    playerEnterWord();
+    compareBothWords();
+    // lettersExistWrongLocation();
+    playerWordsIntoBoxes();
+}
+
+play()
 
 function createBoxes(numBox) {
     for (let i = 0; i < numBox; i++) {
@@ -15,8 +29,6 @@ function createBoxes(numBox) {
     }
 }
 
-createBoxes(5);
-
 function pickRandomWord() {
     let word = words[Math.floor(Math.random() * words.length)]
     charsOfWord = word.split('');
@@ -24,33 +36,41 @@ function pickRandomWord() {
 }
 
 function playerEnterWord() {
-    playerChoice = "bones";
+    playerChoice = prompt("enter a word")
     charsOfChoice = playerChoice.split('');
     return(charsOfChoice);
 }
 
 
-function compareBothWords() {
-    pickRandomWord();
-    playerEnterWord();
+function lettersExistWrongLocation() {
+    const intersection = charsOfChoice.filter(element => charsOfWord.includes(element));
+    console.log(intersection);
+    // intersection.forEach(letter => console.log(charsOfChoice.includes(letter)))
+}
 
+
+function compareBothWords() {   
+    const intersection = charsOfChoice.filter(element => charsOfWord.includes(element)); 
     for (i = 0; i < 5; i++) {
         if (charsOfChoice[i] == charsOfWord[i]) {
             console.log(`this letter is a match: ${charsOfChoice[i]}`);
             document.querySelector(`#box0${i}`).style.backgroundColor = 'green';
+        }  else if () {
+            console.log(`the letter ${charsOfChoice[i]} exists but in the wrong location`);
+            document.querySelector(`#box0${i}`).style.backgroundColor = 'yellow';
         } else {
             console.log(`at position ${i} the random letter was ${charsOfWord[i]} and your letter was ${charsOfChoice[i]}`);
+            // document.querySelector(`#box0${i}`).style.backgroundColor = 'red';
         };
     }
     console.log(`My word is ${charsOfChoice}, the computer choice is ${charsOfWord}`);
 }
 
-compareBothWords();
 
 function playerWordsIntoBoxes() {
     for (let i = 0; i < 5; i++) {
-        document.querySelector(`#box0${i}`).innerText = playerEnterWord()[i];
+        document.querySelector(`#box0${i}`).innerText = charsOfChoice[i];
     }
 }
 
-playerWordsIntoBoxes();
+
